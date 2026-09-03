3521eb45f261f71f343a52ed603fec07bc015830
shahinazkhater <shahinazkhater43@gmail.com>
2026-09-03 23:26:34 +0300

    Create authors CRUD API

--- index.js
+++ index.js
@@ -1,245 +1,62 @@
-import http from "node:http";
+import express from "express";
+import { createDB} from "./db.js";
 
-const users = [
-  { name: "eyad", age: 20 },
-  { name: "shahyinaz", age: 20 },
-];
+const app = express();
+const db= createDB();
 
-// تعريف الموديول
+app.use (express.json());
 
-const server = http.createServer((req, res) => {
+app.use((req,res,next)=>{
+  console.log(new Date().toLocaleString(),req.method,req.url);
+  next();
+})
 
-  const { method, url } = req;
+app.get("/authors",async(req,res)=>{
+  const authors =await db.getAll("authors");
+  const searchQuery = req.query.search;
 
-  // METHOD = post / get
-  // url = users
-
-  if (method === "POST" && url === "/users") {
-    // دا الوضع الصح
-
-    let body = "";
-    // الكلاينت هيبعت بيانات user
-
-    req.on("data", (chunk) => {
-      // كل مجزء من ال requst body حطه فالمتغير body
-
-      // جزء من البيانات.
-
-      // يعني ممكن البيانات توصل كذا جزء:
-
-      // chunk 1
-      // chunk 2
-      // chunk 3
-
-      // وإحنا بنجمعهم:
-
-      // body += chunk;
-
-      // لحد ما يبقى عندنا الـ Body كامل.
-
-      body += chunk;
-    });
-
-    req.on("end", () => {
-      // خلاص، كل الـ body وصل.
-
-      // ساعتها نقدر نتعامل مع البيانات.
-
-      const data = JSON.parse(body);
-
-      users.push(data);
-      // لو فيه عنصر جديد جه ضيفه فالاخر
-
-      res.writeHead(201, {
-        "content-type": "application/json",
-        // هنا السيرفر بيجهز الـ Response.
-
-        // 201 معناها:
-
-        // Created — الحاجة الجديدة اتعملت بنجاح.
-      });
-
-      res.end(
-        JSON.stringify({
-          // JSON.stringify() بتحول الـ JavaScript Object إلى JSON.
-
-          // هنا بنبعت الـ user الجديد للـ client.
-
-          message: "user created successfully",
-          user: data,
-        })
-      );
-    });
+  if (!searchQuery) {
+    return res.json({data: authors});
   }
+  const filteredAuthors = authors.filter((author) =>
+    author.name.toLowerCase().startsWith(searchQuery.toLowerCase())
+  );
+  res.json({data: filteredAuthors});
 
-  // =========================
-  // GET ALL USERS
-  // =========================
+})
 
-  else if (method === "GET" && url === "/users") {
+app.get("/authors/:author_id",async(req,res)=>{
+  const author= await db.getById("authors",req.params.author_id);
+  res.json({data: author});
+})
 
-    res.writeHead(200, {
-      "content-type": "application/json",
-    });
+app.post("/authors",async(req,res)=>{
+ const authorData = req.body;
+ await db.create("authors",authorData);
+ res.status(201).json({message: "Author created successfully", data: authorData});
+})
 
-    // هنا بنبعت كل الـ users للـ client.
-    res.end(JSON.stringify(users));
-  }
+app.patch("/authors/:author_id",async(req,res)=>{
+  const id = req.params.author_id;
+  const author= await db.getById("authors",id);
+  if(!author){return res.status(404).json({ message: "Author not found"})}
+const updateData = req.body;
+await db.update("authors",id,updateData);
+const newauthor = await db.getById("authors",id);  
+return res.status(200).json({message: "Author updated successfully", data: newauthor});
 
-  // =========================
-  // GET SINGLE USER
-  // =========================
+})
 
-  else if (method === "GET" && url.startsWith("/users/")) {
 
-    const userName = url.split("/users/")[1];
+app.delete("/authors/:author_id",async(req,res)=>{
+  const id = req.params.author_id;
+  const author= await db.getById("authors",id);
+  if(!author){return res.status(404).json({ message: "Author not found"})}
+  await db.delete("authors",id);
+  return res.status(200).json({message: "Author deleted successfully"});
+} )
 
-    const user = users.find((u) => u.name === userName);
 
-    if (user) {
-
-      res.writeHead(200, {
-        "content-type": "application/json",
-      });
-
-      res.end(JSON.stringify(user));
-
-    } else {
-
-      res.writeHead(404, {
-        "content-type": "application/json",
-      });
-
-      res.end(
-        JSON.stringify({
-          error: `User ${userName} not found`,
-        })
-      );
-    }
-  }
-
-  // =========================
-  // UPDATE USER
-  // PATCH /users/eyad
-  // PUT /users/eyad
-  // =========================
-
-  else if (
-    (method === "PATCH" || method === "PUT") &&
-    url.startsWith("/users/")
-  ) {
-
-    const userName = url.split("/users/")[1];
-
-    const user = users.find((u) => u.name === userName);
-
-    if (!user) {
-
-      res.writeHead(404, {
-        "content-type": "application/json",
-      });
-
-      return res.end(
-        JSON.stringify({
-          error: `User ${userName} not found`,
-        })
-      );
-    }
-
-    let body = "";
-
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-
-    req.on("end", () => {
-
-      const data = JSON.parse(body);
-
-      // update user
-      Object.assign(user, data);
-
-      res.writeHead(200, {
-        "content-type": "application/json",
-      });
-
-      res.end(
-        JSON.stringify({
-          message: "User updated successfully",
-          user: user,
-        })
-      );
-    });
-  }
-
-  // =========================
-  // DELETE USER
-  // DELETE /users/eyad
-  // =========================
-
-  else if (
-    method === "DELETE" &&
-    url.startsWith("/users/")
-  ) {
-
-    const userName = url.split("/users/")[1];
-
-    const userIndex = users.findIndex(
-      (u) => u.name === userName
-    );
-
-    if (userIndex === -1) {
-
-      res.writeHead(404, {
-        "content-type": "application/json",
-      });
-
-      res.end(
-        JSON.stringify({
-          error: `User ${userName} not found`,
-        })
-      );
-
-    } else {
-
-      const deletedUser = users.splice(userIndex, 1);
-
-      res.writeHead(200, {
-        "content-type": "application/json",
-      });
-
-      res.end(
-        JSON.stringify({
-          message: "User deleted successfully",
-          user: deletedUser[0],
-        })
-      );
-    }
-  }
-
-  // =========================
-  // ROUTE NOT FOUND
-  // =========================
-
-  else {
-
-    res.writeHead(404, {
-      "content-type": "application/json",
-    });
-
-    res.end(
-      JSON.stringify({
-        error: "Route not found",
-      })
-    );
-  }
-
-}); // قفل createServer
-
-// =========================
-// START SERVER
-// =========================
-
-server.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+app.listen(3000,()=>{
+  console.log("Server is running on port 3000");
+})
